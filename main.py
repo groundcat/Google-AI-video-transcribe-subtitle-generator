e6a7bc35a2b6f68e101894e76e7b46149e2151d8
groundcat <11240246+groundcat@users.noreply.github.com>
2023-09-05 21:41:39 -0400

    Fix timeout error on file upload
    Fix timeout error on file upload #2

--- main.py
+++ main.py
@@ -31,13 +31,13 @@ def main():
     write_srt(response)
     write_txt(response)
 
-def upload_blob(bucket_name, source_file_name, destination_blob_name):
+def upload_blob(bucket_name, source_file_name, destination_blob_name, timeout=None):
     """Uploads a file to the bucket."""
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
 
-    blob.upload_from_filename(source_file_name)
+    blob.upload_from_filename(source_file_name, timeout=timeout)
 
     print(
         "File {} uploaded to {}.".format(
